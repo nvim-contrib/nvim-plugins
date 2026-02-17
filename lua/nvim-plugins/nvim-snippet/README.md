4c6e2a5a047556351bfd8a368e8b194b2dc7f67c
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-02-17 07:55:22 +0400

    feat(nvim-snippet): add LuaSnip configuration and README
    A new configuration for the LuaSnip snippet framework has been added to
    the Neovim plugins directory. This includes:
    
    - Creating an initial README.md with project description and repository
      link
    - Setting up the plugin configuration in init.lua
    - Configuring snippet loading from the nvim-snippets directory

---
+++ lua/nvim-plugins/nvim-snippet/README.md
@@ -0,0 +1,5 @@
+# nvim-snippet
+
+An extensible framework for interacting with snippets within Neovim.
+
+**repository:** <https://github.com/L3MON4D3/LuaSnip>
---
+++ lua/nvim-plugins/nvim-snippet/init.lua
@@ -0,0 +1,14 @@
+return {
+  "L3MON4D3/LuaSnip",
+  dependencies = {
+    "nvim-contrib/nvim-snippets",
+  },
+  config = function(plugin, opts)
+    -- include the default astronvim config that calls the setup call
+    require("astronvim.plugins.configs.luasnip")(plugin, opts)
+    -- load snippets paths
+    require("luasnip.loaders.from_vscode").lazy_load({
+      paths = { vim.fn.stdpath("data") .. "/lazy/nvim-snippets" },
+    })
+  end,
+}
